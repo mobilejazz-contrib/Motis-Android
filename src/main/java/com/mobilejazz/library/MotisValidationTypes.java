3f14da014425d2d6988213c0f95118e77f0fa993
Jose Luis  Franconetti Olmedo <joseluis.franconetti@gmail.com>
2014-10-10 18:40:18 +0200

    Added MotisValidationTypes

---
+++ src/main/java/com/mobilejazz/library/MotisValidationTypes.java
@@ -0,0 +1,14 @@
+package com.mobilejazz.library;
+
+/**
+ * Created by Jose Luis on 10/10/14.
+ */
+public class MotisValidationTypes {
+
+
+    public static String removedAllSpaces (String value) {
+        return value.replaceAll("\\s","");
+    }
+
+
+}
